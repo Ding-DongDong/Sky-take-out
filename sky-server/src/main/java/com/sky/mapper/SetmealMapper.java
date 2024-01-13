2a4dac8af02a58e5c335e603d81af9f37b6a52bd
“dingrundong <2296596872@qq.com>
2024-01-13 12:30:34 +0800

    update .gitignore

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sky.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-@Mapper
-public interface SetmealMapper {
-    @Select("select count(id) from setmeal where category_id = #{categoryId}")
-    Integer CountByCategoryId(Long categoryId);
-}
